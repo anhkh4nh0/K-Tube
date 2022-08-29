04d64aa370cc62ef8d4871dd60e18989d00b6ccc
KhanhNguyen <khanhdesignit@gmail.com>
2022-08-29 16:20:13 +0700

    Update firebase.js

--- FrontEnd/src/firebase.js
+++ FrontEnd/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_APIKEY,
-	authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-	projectId: process.env.NEXT_PUBLIC_PROJECTID, 
-	storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-	appId: process.env.NEXT_PUBLIC_APPID
+	apiKey: "AIzaSyDlCRlk8QTeiNbFexHFRTExepMx4A3qOsQ",
+	authDomain: "clone-977b7.firebaseapp.com",
+	projectId: "clone-977b7",
+	storageBucket: "clone-977b7.appspot.com",
+	messagingSenderId: "632219022837",
+	appId: "1:632219022837:web:433ec447801da1dc855cbc"
 };
 
 const app = initializeApp(firebaseConfig);
